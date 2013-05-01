77726a404a6e399eb2a2552f486f22968aeacf43
rshiva <nadarshiva.r@gmail.com>
2013-05-01 17:54:49 +0530

    indentation fix

--- user_tweets.rb
+++ user_tweets.rb
@@ -16,9 +16,11 @@ Shoes.app do
     	  @twitter_url = "http://api.twitter.com/1/statuses/user_timeline.json?count=10&screen_name="
     	  remote_full_url = @twitter_url + @twitter_name.text
         
+
         tweets = open(remote_full_url).read
 				tweets_to_json=JSON(tweets)
    			tweets_to_json.first["text"]
+        
    			@tweet_stack=stack(:margin => 20) do 
 					tweets_to_json.each do |tweet|
 				 	@tweet_stack =	para tweet["text"], :weight => "bold", :font => "serif 16px"

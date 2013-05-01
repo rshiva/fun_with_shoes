71c0c3b714264c1f7aa021b2cd0a6f6ea2683633
Shiva kumar <nadarshiva.r@gmail.com>
2013-05-01 17:55:45 +0530

    Update user_tweets.rb
    removed comment..text font

--- user_tweets.rb
+++ user_tweets.rb
@@ -1,19 +1,6 @@
 require "open-uri"
  require "JSON"
 
-# puts "Enter you twitter name"
-# twitter_user=ARGV.gets
-# remote_base_url = "http://api.twitter.com/1/statuses/user_timeline.json?count=10&screen_name="
-# remote_full_url = remote_base_url + twitter_user
-
-# tweets = open(remote_full_url).read
-# tweets_to_json=JSON(tweets)
-# tweets_to_json.first["text"]
-# tweets_to_json.each do |tweet|
-# 	tweet["text"]
-# end
-
-
 Shoes.app do
   background "#c0deed"
   border(
@@ -34,7 +21,7 @@ Shoes.app do
    			tweets_to_json.first["text"]
    			@tweet_stack=stack(:margin => 20) do 
 					tweets_to_json.each do |tweet|
-				 	@tweet_stack =	para tweet["text"]
+				 	@tweet_stack =	para tweet["text"], :weight => "bold", :font => "serif 16px"
  					end
  				end
       end
